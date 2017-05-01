07599783f0ada4a3d7a0aa2998619b7e8c5d8fac
Katarina1330 <Katarina@Katarinas-MacBook-Pro.local>
2017-05-01 12:46:53 +0200

    Read all articles and update article

--- articles/articleController.js
+++ articles/articleController.js
@@ -1,6 +1,23 @@
-app.controller('articleController', function($scope, articleService){
+app.controller('articleController', function ($scope, articleService) {
 
     $scope.articleService = articleService;
 
     $scope.articleService.getArticles();
+
+    $scope.toggleEditArticle = function (article) {
+        article.editArticle = !article.editArticle;
+    }
+
+    $scope.editArticle = function (article) {
+        var article = {
+            id: article.id,
+            poster: 1,
+            body: article.body,
+            title: article.title,
+            datetime: new Date()
+        }
+
+        $scope.articleService.editArticle(article);
+    }
+
 })
--- articles/articleService.js
+++ articles/articleService.js
@@ -1,15 +1,28 @@
-app.service('articleService', function($http){
+app.service('articleService', function ($http) {
 
     var self = this;
     self.$http = $http;
 
-// Read all Articles
-self.getArticles = function(){
-    $http.get("http://www.scripttic.com:8000/api/v1/article").then(function(response){
+    // Read all Articles
+    self.getArticles = function () {
+        var getArticlePath = "http://www.scripttic.com:8000/api/v1/article"
+        $http.get(getArticlePath).then(function (response) {
             self.articles = response.data;
-    }, function(error){
-        console.log(error);
-    })
-}
+        }, function (error) {
+            console.log(error);
+        })
+    }
+
+    // Update Article
+    self.editArticle = function (article) {
+        var editArticlePath = "http://www.scripttic.com:8000/api/v1/article";
+        $http.defaults.headers.common['Authorization'] = 'Bearer ' + 'ecb75277-09d9-42e9-ba75-2458eb39b76e';
+        $http.put(editArticlePath, article).then(function(response){
+
+        }, function(error){
+            console.log(error);
+        })
+    }
+
 
 })
