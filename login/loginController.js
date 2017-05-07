77a41ccd65bca5e528f5c6f62a9d056153b6f528
Katarina1330 <Katarina@Katarinas-MacBook-Pro.local>
2017-05-07 16:50:35 +0200

    Added login.html, loginController.js and loginService.js

--- login/loginController.js
+++ login/loginController.js
@@ -1,4 +1,9 @@
 app.controller('loginController', function($scope, loginService){
 
     $scope.loginService = loginService;
+
+    $scope.login = function(email, password){
+
+        loginService.login(email, password);
+    }
 }) 
--- login/loginService.js
+++ login/loginService.js
@@ -1,7 +1,31 @@
-app.service('loginService', function($http){
+app.service('loginService', function($http, $httpParamSerializerJQLike){
 
     var self = this;
     self.$http = $http;
+    self.$httpParamSerializerJQLike = $httpParamSerializerJQLike;
 
-    
+     // Login User
+    self.login = function (email, password) {
+
+            var data = {'email': email, 'password': password, 'grant_type': 'Bearer'};
+
+            $http({
+                url: 'http://www.scripttic.com:8000/oauth2/token',
+                method: 'POST',
+                data: $httpParamSerializerJQLike(data),
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(function (response) {
+                    // success
+                    var token = response.data;
+
+                    window.history.back();
+                    console.log(response);
+                 },
+                 function (response) {
+                     // failed
+                     console.log(response);
+                 });
+    }
 }) 
